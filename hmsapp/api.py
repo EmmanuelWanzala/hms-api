af535ff9d803b9832dfd279e80e1e304da3e7014
ibukamshindi <ibukamshindi@gmail.com>
2020-11-12 20:46:36 +0300

    create hmsapp appointment api end points

--- hmsapp/api.py
+++ hmsapp/api.py
@@ -1,2 +1,20 @@
 from rest_framework.response import Response
 from rest_framework import generics,permissions
+from .serializer import AppointmentSerializer
+from .models import Appointment
+
+class AppointmentCreateApi(generics.CreateAPIView):
+    queryset= Appointment.objects.all()
+    serializer_class=AppointmentSerializer
+
+class AppointmentApi(generics.ListAPIView):
+    queryset=Appointment.objects.all()
+    serializer_class=AppointmentSerializer
+
+class AppointmentUpdateApi(generics.RetrieveUpdateAPIView):
+    queryset=Appointment.objects.all()
+    serializer_class=AppointmentSerializer
+
+class AppointmentDeleteApi(generics.DestroyAPIView):
+    queryset=Appointment.objects.all()
+    serializer_class=AppointmentSerializer
--- hmsapp/urls.py
+++ hmsapp/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from .api import AppointmentCreateApi,AppointmentApi, AppointmentDeleteApi,AppointmentUpdateApi
+
 # from .api import AppointmentAPI
 
 urlpatterns = [
+      path('api/appointment/create',AppointmentCreateApi.as_view()),
+      path('api/appointment',AppointmentApi.as_view()),
+      path('api/appointment/<int:pk>',AppointmentUpdateApi.as_view()),
+      path('api/appointment/<int:pk>',AppointmentDeleteApi.as_view()),
+
+
     # path('api/appointment',ScheduleAPI.as_view()),
     
 ]
