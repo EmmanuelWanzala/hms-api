a6abce362fbce371ccf852ec8ac7f236a7098914
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-12 17:11:54 +0300

    Create customuser model from auth models abstractuser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+from .managers import CustomUserManager
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+
+    # These fields tie to the roles!
+    ADMIN = 1
+    DOCTOR = 2
+    PATIENT = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (DOCTOR, 'Doctor'),
+        (PATIENT, 'Patient')
+    )
+
+    username = None
+    email = models.EmailField(('email address'), unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
