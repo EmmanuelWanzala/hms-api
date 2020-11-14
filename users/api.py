1803cc4d8771c0862693b423a1e62d69962a7c28
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-14 16:45:18 +0300

    Create accounts activate view function and endpoint

--- users/api.py
+++ users/api.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect,reverse
 from rest_framework import generics, permissions, mixins,status
 from rest_framework.response import Response
+from django.contrib.auth import login, authenticate
 from .serializer import UserRegistrationSerializer
 from .models import CustomUser as User
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .token_generator import account_activation_token
 #Register API
 class RegisterApi(generics.GenericAPIView):
     serializer_class = UserRegistrationSerializer
@@ -24,3 +29,28 @@ class RegisterApi(generics.GenericAPIView):
             }
 
             return Response(response, status=status_code)
+
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        context={
+        'success':True,
+        'status':'success',
+        'message':'Your account has been activated successfully',
+        }
+        return render(request,'activation_status.html',context)
+    else:
+        context={
+        'success':False,
+        'status':'danger',
+        'message':'Activation link is invalid!',
+        }
+        return render(request,'activation_status.html',context)       
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.urls import path, include
-from .api import RegisterApi
+from .api import RegisterApi,activate_account
 urlpatterns = [
       path('api/register', RegisterApi.as_view()),
+      path('activate/<uidb64>/<token>',activate_account, name='activate'),
 ]
