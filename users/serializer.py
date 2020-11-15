fc560c5177fe8bc9af0f4a260c13ca2cbe339ec3
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-15 11:14:12 +0300

    Create userlogin serializer

--- users/serializer.py
+++ users/serializer.py
@@ -2,6 +2,10 @@ from rest_framework import  serializers
 from django.db import models
 from .models import CustomUser as User
 from django.contrib.auth.hashers import make_password
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+
 # Register serializer
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +17,46 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
       user = User.objects.create_user(**validated_data)
       return user
+
+
+# Login serializer
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+    access = serializers.CharField(read_only=True)
+    refresh = serializers.CharField(read_only=True)
+    role = serializers.CharField(read_only=True)
+
+    def create(self, validated_date):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+    def validate(self, data):
+        email = data['email']
+        password = data['password']
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid login credentials")
+        elif user and not user.is_active:
+            raise serializers.ValidationError("Verify your email address to login")
+
+        try:
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh)
+            access_token = str(refresh.access_token)
+
+            update_last_login(None, user)
+
+            validation = {
+                'access': access_token,
+                'refresh': refresh_token,
+                'email': user.email,
+                'role': user.role,
+            }
+
+            return validation
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Invalid login credentials")      
