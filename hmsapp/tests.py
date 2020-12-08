e583c00fae3e4fb4a0a41984aab583dda1b5962b
Dennis Kamunya <dennismuriithik@gmail.com>
2020-12-08 11:58:35 +0300

    Create service api views test cases

--- hmsapp/tests.py
+++ hmsapp/tests.py
@@ -30,3 +30,62 @@ class ServiceTestClass(TestCase):
         self.assertTrue(isinstance(self.new_service,Service))    
 
 
+class ServiceViewTestCase(TestCase):
+    """Test suite for the service model api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.service=Service.objects.create(service_name='X-ray',service_cost=500,service_description='test')
+        self.service_data = {'service_name': 'Consultant','service_cost':1500,'service_description':'test'}
+        
+
+    def test_api_can_create_a_service(self):
+        """Test the api can create a service."""
+        response = self.client.post(
+            reverse('service_create'),
+            self.service_data,
+            format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+    def test_api_can_get_all_service(self):
+        """ Test the api can get all services """
+        # get API response
+        response = self.client.get(reverse('service_create'))
+        # get data from db
+        services = Service.objects.all()
+        serializer = ServiceSerializer(services, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_api_can_get_a_service(self):
+        """Test the api can get a given service."""
+        
+        response = self.client.get(
+            reverse('service_update',
+            kwargs={'pk': self.service.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.service)
+
+    def test_api_can_update_service(self):
+        """Test the api can update a given service."""
+        change_service = {'service_name': 'Consultant','service_cost':500,'service_description':'test'}
+        
+        res = self.client.put(
+            reverse('service_update', kwargs={'pk': self.service.id}),
+            change_service, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_service(self):
+        """Test the api can delete a service."""
+        response = self.client.delete(
+            reverse('service_update', kwargs={'pk': self.service.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
